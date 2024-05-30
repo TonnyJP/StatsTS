400b1478b6b1a6b7f0520557a4c4add21e6d0f0b
Tonny Piper <pipertonny@gmail.com>
2024-05-30 11:36:20 +0200

    WIP

---
+++ src/MatchResult.ts
@@ -0,0 +1,5 @@
+export enum MatcheResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import { CsvFileReader } from "./CsvFileReader";
+import { MatcheResult } from "./MatchResult";
+
+const matches = new CsvFileReader("football.csv");
+
+let manUnitedWins = 0;
+matches.read();
+
+for (let match of matches.data) {
+  if (match[1] === "Man United" && match[5] === MatcheResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatcheResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United wins ${manUnitedWins} games.`);
