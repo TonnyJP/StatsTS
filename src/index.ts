334a2877858d8d0dd469fb1b8ae8b9637fe6a26c
Tonny Piper <pipertonny@gmail.com>
2024-05-31 09:42:28 +0200

    Some generics

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,50 @@ const summray = Summary.winsAnalysisWithHtmlReport("Man United"); //new Summary(
 
 summray.buildAndPrintReport(matchReader.matches);
 //Summary.winsAnalysisWithHtmlReport("Man United");
+
+//more about Generics
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const numberCollection = new ArrayOfAnything<number>([1, 2, 6, 10, 15]);
+console.log(numberCollection.get(3));
+const stringCollection = new ArrayOfAnything<string>(["a", "Z", "r", "s"]);
+console.log(stringCollection.collection);
+
+//---------------
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+/* printAnything<string>(["h", "a", "l", "l", "o", " ", "mo", "tto"]);
+printAnything<number>([39, 34, 12, 4, 67, 23]);
+printAnything<boolean>([true, true, false, true, false, false]); */
+
+//------------
+interface Printable {
+  print(): void;
+}
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+class House {
+  print(): void {
+    console.log("I am a House");
+  }
+}
+function printer<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printer([new Car(), new Car(), new House(), new Car(), new House()]);
